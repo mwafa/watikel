c68da07ff2050e1bcd46edefb33ed14103dfd882
Muhammad Wafa <wafax.4@gmail.com>
2021-06-09 09:23:56 +0700

    working add new task

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,7 +15,7 @@ import {
   DrawerOverlay,
 } from "@chakra-ui/modal"
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu"
-import React, { useContext } from "react"
+import React, { FormEventHandler, useContext, useState } from "react"
 
 import { Input } from "@chakra-ui/input"
 import { LastUpdateCtx } from "../contexts/last-update"
@@ -28,6 +28,28 @@ export const MainMenu = () => {
   const { update } = useContext(LastUpdateCtx)
   const { isOpen, onOpen, onClose } = useDisclosure()
   const router = useRouter()
+  const [loading, setLoading] = useState<boolean>(false)
+  const [title, setTitle] = useState<string>()
+  const [reference, setReference] = useState<string>()
+
+  const clear = () => {
+    setTitle("")
+    setReference("")
+  }
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    fetcher("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, reference }),
+    }).finally(() => {
+      clear()
+      onClose()
+      update()
+      setLoading(false)
+    })
+  }
   return (
     <Menu>
       <MenuButton
@@ -53,28 +75,54 @@ export const MainMenu = () => {
           Log out
         </MenuItem>
       </MenuList>
-
       <Drawer isOpen={isOpen} placement="bottom" onClose={onClose}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader fontSize="md">New Task</DrawerHeader>
+          <form onSubmit={onSubmit}>
+            <DrawerBody>
+              <VStack spacing="4">
+                <Input
+                  size="sm"
+                  variant="flushed"
+                  placeholder="Title"
+                  value={title}
+                  onChange={({ target }) => setTitle(target.value)}
+                  required
+                />
+                <Input
+                  size="sm"
+                  variant="flushed"
+                  placeholder="Reference"
+                  value={reference}
+                  onChange={({ target }) => setReference(target.value)}
+                />
+              </VStack>
+            </DrawerBody>
 
-          <DrawerBody>
-            <VStack spacing="4">
-              <Input size="sm" variant="flushed" placeholder="Title" />
-              <Input size="sm" variant="flushed" placeholder="Reference" />
-            </VStack>
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button size="sm" variant="outline" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button size="sm" colorScheme="orange">
-              Save
-            </Button>
-          </DrawerFooter>
+            <DrawerFooter>
+              <Button
+                size="sm"
+                variant="outline"
+                mr={3}
+                onClick={() => {
+                  onClose()
+                  clear()
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                size="sm"
+                type="submit"
+                colorScheme="orange"
+                isLoading={loading}
+              >
+                Save
+              </Button>
+            </DrawerFooter>
+          </form>
         </DrawerContent>
       </Drawer>
     </Menu>
